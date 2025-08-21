61f5124b116ee5c0e53fbe51380f896db6aadb5b
BlasBlas926 <Blasito029@gmail.com>
2025-08-21 17:13:20 -0600

    Pruebas

--- Codigo/ECE/obj/Debug/net6.0/ECE.AssemblyInfo.cs
+++ Codigo/ECE/obj/Debug/net6.0/ECE.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ECE")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+29edffda54482fb8a3b92afdfec3348ce536011c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ec108ef0252d527abe1111a35643fb8d9d006a11")]
 [assembly: System.Reflection.AssemblyProductAttribute("ECE")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ECE")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
